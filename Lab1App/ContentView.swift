4104a8b7e5c917da5cd351aa3837b0c9919ba6c1
billie-lee <billieanne.lee@me.com>
2022-03-14 23:10:32 +1000

    Initial Commit

---
+++ Lab1App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab1App
+//
+//  Created by Billie Anne Lee on 14/3/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab1App/Lab1AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1AppApp.swift
+//  Lab1App
+//
+//  Created by Billie Anne Lee on 14/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct Lab1AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
